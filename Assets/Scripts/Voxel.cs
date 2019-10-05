06fd2959edd9dce1a25274a98ca69cf098a86948
tomthecarrot <code@tomthecarrot.com>
2019-10-04 23:08:29 -0700

    Create Voxel class

---
+++ Assets/Scripts/Voxel.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Teleportal;
+
+public class Voxel : MonoBehaviour
+{
+    private TPObject tpo;
+    private MeshRenderer renderer;
+
+    void Awake() {
+        this.renderer = this.GetComponent<MeshRenderer>();
+    }
+
+    void Start() {
+        this.tpo = TPObject.get(this.transform.parent.gameObject);
+        this.tpo.Subscribe("explored", this.OnExplored);
+    }
+
+    public void Explore() {
+        // Claim as self on Realm
+        this.tpo.SetState("explored", Teleportal.Teleportal.tp.GetUsername());
+
+        // (add here)
+    }
+
+    private void OnExplored(string newValue) {
+        this.renderer.enabled = true;
+    }
+}
