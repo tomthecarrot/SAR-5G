ffea791381a15ea6191a1df18368810dda45cd68
John <johnhanacek@gmail.com>
2019-10-05 12:12:52 -0700

    experimenting with grid placement ux

---
+++ Assets/Scripts/CubePlacer.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class CubePlacer : MonoBehaviour
+{
+    public GameObject Marker;
+    private Grid grid;
+
+    private void Awake()
+    {
+        grid = FindObjectOfType<Grid>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hitInfo;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hitInfo))
+            {
+                PlaceCubeNear(hitInfo.point);
+            }
+        }
+    }
+
+    private void PlaceCubeNear(Vector3 clickPoint)
+    {
+        var finalPosition = grid.GetNearestPointOnGrid(clickPoint);
+        GameObject.Instantiate(Marker).transform.position = finalPosition;
+
+        //GameObject.CreatePrimitive(PrimitiveType.Sphere).transform.position = nearPoint;
+    }
+}
---
+++ Assets/Scripts/Grid.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class Grid : MonoBehaviour
+{
+    [SerializeField]
+    private float size = 1f;
+
+    public Vector3 GetNearestPointOnGrid(Vector3 position)
+    {
+        position -= transform.position;
+
+        int xCount = Mathf.RoundToInt(position.x / size);
+        int yCount = Mathf.RoundToInt(position.y / size);
+        int zCount = Mathf.RoundToInt(position.z / size);
+
+        Vector3 result = new Vector3(
+            (float)xCount * size,
+            (float)yCount * size,
+            (float)zCount * size);
+
+        result += transform.position;
+
+        return result;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        for (float x = 0; x < 40; x += size)
+        {
+            for (float z = 0; z < 40; z += size)
+            {
+                var point = GetNearestPointOnGrid(new Vector3(x, 0f, z));
+                Gizmos.DrawSphere(point, 0.1f);
+            }
+
+        }
+    }
+}
