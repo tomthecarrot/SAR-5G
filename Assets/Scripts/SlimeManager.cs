98ee8ae2de424f5be3775902a753f7d3cb1a24d9
tomthecarrot <code@tomthecarrot.com>
2019-10-04 23:08:22 -0700

    Create SlimeManager class

---
+++ Assets/Scripts/SlimeManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Teleportal;
+
+public class SlimeManager : MonoBehaviour
+{
+    public string GodUser = "NULL";
+    private TPObject tpo;
+
+    void Start() {
+        this.tpo = TPObject.get(this.transform.parent.gameObject);
+        this.tpo.Subscribe("god", this.OnSetGod);
+    }
+
+    private void OnSetGod(string newGod) {
+        // Set global variable
+        this.GodUser = newGod;
+
+        // Update UI
+        bool isSelf = (newGod == Teleportal.Teleportal.tp.GetUsername());
+        GodIndicator.Shared.gameObject.SetActive(isSelf);
+    }
+
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.G)) {
+            this.ToggleGod();
+        }
+    }
+
+    public void ToggleGod() {
+        string newGod = Teleportal.Teleportal.tp.GetUsername();
+        if (this.GodUser == Teleportal.Teleportal.tp.GetUsername()) {
+            newGod = "NULL";
+        }
+
+        this.tpo.SetState("god", newGod);
+    }
+}
